d739aa64e73e0b11ffbae8a61d633a8a3af58b15
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-08-30 14:09:12 +0300

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 0
+        if num == 12
+            return "HAPPY NEW YEAR!"
+        else 
+            puts  "#{num} SECOND(S)!"
+        end     
+        num -= 1
+    end     
 end
+
+def countdown_with_sleep(num)
+    while num > 0
+        sleep num 
+        return "#{num}"
+        num -= 1
+    end     
+end 
